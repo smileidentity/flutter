07fb908f45d1a7fef489e8bbcae0db7f0dc038f9
Juma Allan <allanjuma@gmail.com>
2023-07-18 11:16:35 +0300

    fixed test import

--- sample/test/widget_test.dart
+++ sample/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:smileid_sample/main.dart';
+import 'package:sample/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
