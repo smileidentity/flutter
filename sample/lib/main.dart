b4668ed0ae60f29cd49d59a14eb53b21f95cf5af
Juma Allan <allanjuma@gmail.com>
2023-07-27 19:21:35 +0300

    working on sample app

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:smileid/messages.g.dart';
 import 'package:smileid/smileid.dart';
 
 void main() {
@@ -27,12 +28,16 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    _smileidPlugin.initialize();
+
+    // _smileidPlugin.doEnhancedKycAsync( FlutterEnhancedKycRequest(country: country, idType: idType, idNumber: idNumber, firstName: firstName, middleName: middleName, lastName: lastName, dob: dob, phoneNumber: phoneNumber, bankCode: bankCode, callbackUrl: callbackUrl, partnerParams: partnerParams, partnerId: partnerId, sourceSdk: sourceSdk, sourceSdkVersion: sourceSdkVersion, timestamp: timestamp, signature: signature))
+
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _smileidPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _smileidPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
