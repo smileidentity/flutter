483845869886378c149e9b8f989df475da259335
Juma Allan <allanjuma@gmail.com>
2023-08-08 16:53:54 +0300

    updated tests and updated build file

--- android/src/test/kotlin/com/smileidentity/flutter/SmileIDPluginTest.kt
+++ android/src/test/kotlin/com/smileidentity/flutter/SmileIDPluginTest.kt
@@ -1,9 +1,15 @@
 package com.smileidentity.flutter
 
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
+import FlutterAuthenticationRequest
+import FlutterAuthenticationResponse
+import FlutterEnhancedKycAsyncResponse
+import FlutterEnhancedKycRequest
+import SmileIDApi
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
 import kotlin.test.Test
-import org.mockito.Mockito
 
 /*
  * This demonstrates a simple unit test of the Kotlin portion of this plugin's implementation.
@@ -12,17 +18,63 @@ import org.mockito.Mockito
  * line by running `./gradlew testDebugUnitTest` in the `example/android/` directory, or
  * you can run them directly from IDEs that support JUnit such as Android Studio.
  */
-
 internal class SmileIDPluginTest {
 
-  @Test
-  fun onMethodCall_getPlatformVersion_returnsExpectedValue() {
-    val plugin = SmileIDPlugin()
+    @Test
+    fun `when we call authenticate and pass a request object, we get a successful callback`() {
 
-    val call = MethodCall("getPlatformVersion", null)
-    val mockResult: MethodChannel.Result = Mockito.mock(MethodChannel.Result::class.java)
-    plugin.onMethodCall(call, mockResult)
+        val request = mockk<FlutterAuthenticationRequest>()
+        val callback = mockk<(Result<FlutterAuthenticationResponse>) -> Unit>()
+        val api = mockk<SmileIDApi>()
 
-    Mockito.verify(mockResult).success("Android " + android.os.Build.VERSION.RELEASE)
-  }
+        coEvery {
+            api.authenticate(
+                request = request,
+                callback = callback
+            )
+        } returns Unit
+
+        api.authenticate(
+            request = request,
+            callback = callback
+        )
+
+        coVerify {
+            api.authenticate(
+                request = request,
+                callback = callback
+            )
+        }
+
+        confirmVerified(api)
+    }
+
+    @Test
+    fun `when we call doEnhancedKycAsync and pass a request object, we get a successful callback`() {
+
+        val request = mockk<FlutterEnhancedKycRequest>()
+        val callback = mockk<(Result<FlutterEnhancedKycAsyncResponse>) -> Unit>()
+        val api = mockk<SmileIDApi>()
+
+        coEvery {
+            api.doEnhancedKycAsync(
+                request = request,
+                callback = callback
+            )
+        } returns Unit
+
+        api.doEnhancedKycAsync(
+            request = request,
+            callback = callback
+        )
+
+        coVerify {
+            api.doEnhancedKycAsync(
+                request = request,
+                callback = callback
+            )
+        }
+
+        confirmVerified(api)
+    }
 }
