69cbea6ab9a5bf025151c8c9b2fc88221ce07baa
Vansh Gandhi <vansh@smileidentity.com>
2023-09-20 14:27:47 -0700

    Clean up

--- android/src/main/kotlin/com/smileidentity/flutter/SmileIDDocumentVerification.kt
+++ android/src/main/kotlin/com/smileidentity/flutter/SmileIDDocumentVerification.kt
@@ -17,6 +17,7 @@ import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 import java.io.File
+import java.lang.RuntimeException
 
 internal class SmileIDDocumentVerification private constructor(
     context: Context,
@@ -32,14 +33,8 @@ internal class SmileIDDocumentVerification private constructor(
     private val view: View
 
     init {
-        // print contents of args
-        args.forEach { (key, value) ->
-            Log.d("SmileIDDocumentVerification", "$key: $value")
-        }
         methodChannel = MethodChannel(messenger, "SmileIDDocumentVerification_$viewId")
         view = ComposeView(context).apply {
-            Log.d("SmileIDDocumentVerification", "getView")
-            // setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 SmileID.DocumentVerification(
                     // TODO: Global DocV changes
@@ -90,31 +85,11 @@ internal class SmileIDDocumentVerification private constructor(
 
     override fun getView() = view
 
-    override fun dispose() {
-        // view = null
-        Log.d("SmileIDDocumentVerification", "dispose")
-    }
+    override fun dispose() = Unit
 
     override fun onFlutterViewAttached(flutterView: View) {
-        super.onFlutterViewAttached(flutterView)
-        // view?.setViewTreeViewModelStoreOwner(flutterView.findViewTreeViewModelStoreOwner())
-        Log.d("SmileIDDocumentVerification", "onFlutterViewAttached")
-    }
-
-    override fun onFlutterViewDetached() {
-        super.onFlutterViewDetached()
-        // view?.setViewTreeViewModelStoreOwner(null)
-        Log.d("SmileIDDocumentVerification", "onFlutterViewDetached")
-    }
-
-    override fun onInputConnectionLocked() {
-        super.onInputConnectionLocked()
-        Log.d("SmileIDDocumentVerification", "onInputConnectionLocked")
-    }
-
-    override fun onInputConnectionUnlocked() {
-        super.onInputConnectionUnlocked()
-        Log.d("SmileIDDocumentVerification", "onInputConnectionUnlocked")
+        // Log.e("SmileIDDocumentVerification", "flutterView: $flutterView")
+        throw RuntimeException("crash")
     }
 
     class Factory(
