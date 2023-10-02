4cd7c127c13d153f5eefa1a4681b6cb119c05291
Juma Allan <allanjuma@gmail.com>
2023-10-02 18:51:13 +0300

    added linter to sample android app

--- example/android/app/src/main/kotlin/com/smileidentity/flutter/sample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/smileidentity/flutter/sample/MainActivity.kt
@@ -2,4 +2,4 @@ package com.smileidentity.flutter.sample
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity()
