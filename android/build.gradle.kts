c33ba4e47aac2dd4a5d1bebccce91d00ef556718
Juma Allan <allanjuma@gmail.com>
2025-06-09 20:29:01 +0400

    reworked gradle files to fix build failures

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,76 @@
 plugins {
-    // Applied to all sub-modules
-    alias(libs.plugins.ktlint)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 
-    // Applied depending on sub-module
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.compose.compiler)
 }
 
-tasks.create("clean", Delete::class.java) {
-    delete(rootProject.layout.buildDirectory)
+allprojects {
+    repositories {
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
+    }
+}
+
+android {
+    namespace = "com.smileidentity.flutter"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+
+        // Read version from pubspec.yaml for setWrapperInfo
+        val pubspecYaml = File("../pubspec.yaml")
+        val pubspecText = pubspecYaml.readText()
+        val versionRegex = Regex("""version:\s*(.+)""")
+        val versionMatch = versionRegex.find(pubspecText)
+        val version = if (versionMatch != null) {
+            pubspecText.split(Regex("""version:\s*"""))[1].split("\n")[0].trim()
+        } else {
+            "11.0.0"
+        }
+        buildConfigField("String", "SMILE_ID_VERSION", "\"$version\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += listOf("-Xskip-metadata-version-check") // metadata version check skip flag
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+    }
+
+    lint {
+        disable.add("NullSafeMutableLiveData")
+    }
+
+    buildFeatures.compose = true
+}
+
+dependencies {
+    implementation(libs.smileid)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.viewmodel)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.fragment)
+    implementation(libs.kotlin.coroutines)
+    implementation(libs.kotlin.immutable.collections)
+    implementation(libs.mlkit)
+
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.mockk)
 }
 
 ktlint {
--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -1,10 +1,3 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ktlint) apply false
-    alias(libs.plugins.flutter.plugin.loader) apply false
-}
-
 rootProject.layout.buildDirectory.set(file("../build"))
 
 subprojects {
