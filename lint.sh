b6b5ee6406fe599a91f9dd6ab1a922c083bf2cdb
Juma Allan <allanjuma@gmail.com>
2025-06-18 03:14:37 +0300

    update dart linter

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
-dart format . && cd sample/android && ./gradlew ktlintFormat && cd ../.. && cd android && ./gradlew ktlintFormat
+dart format . --set-exit-if-changed && flutter analyze && cd sample/android && ./gradlew ktlintFormat && cd ../.. && cd android && ./gradlew ktlintFormat
 
 # Before use it, in the first time, you must guarantee some running permissions:
 # chmod +x lint.sh
