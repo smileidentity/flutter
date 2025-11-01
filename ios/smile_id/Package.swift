639805cf4eb63eb555f27ccd50db298a27e9f18d
Juma Allan <allanjuma@gmail.com>
2025-11-01 22:31:08 +0200

    fixed spm setup for flutter

--- ios/smile_id/Package.swift
+++ ios/smile_id/Package.swift
@@ -7,5 +7,23 @@ let package = Package(
     platforms: [
         .iOS("13.0")
     ],
-    dependencies: []
+    products: [
+        .library(name: "smile-id", targets: ["smile_id"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/smileidentity/ios.git", .upToNextMajor(from: "11.1.1"))
+    ],
+    targets: [
+        .target(
+            name: "smile_id",
+            dependencies: [
+                "SmileIDSDK"
+            ]
+        ),
+        .binaryTarget(
+            name: "SmileIDSDK",
+            url: "https://github.com/smileidentity/ios/releases/download/v11.1.2/SmileIDSDK.xcframework.zip",
+            checksum: "623d441897a824e7bf7ed41ac55565d27bc266d4f5872ff94f40043dd7654b42"
+        )
+    ]
 )
